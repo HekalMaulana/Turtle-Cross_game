b9cf299df7b123f7748162744929e7e109d4a981
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2023-07-07 13:04:05 +0800

    turtle-player-sub-main-program
    -Adding turtle
    -Adding move condition when user press Up keyword in his keyboard

---
+++ player.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.start_position()
+
+    def start_position(self):
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+        y = self.ycor() + MOVE_DISTANCE
+        x = self.xcor()
+        self.goto(x, y)
